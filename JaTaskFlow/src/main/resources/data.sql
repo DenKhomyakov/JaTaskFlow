aa09bd7e94c76b4e0ec1ae905b7825a13d428ec6
Denis Khomyakov <Homyakov.Denis2021@yandex.ru>
2025-03-30 21:01:19 +0700

    Added test data for the User

---
+++ JaTaskFlow/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO user (firstname, lastname, email, password, role)
+VALUES
+    ('Admin', 'Adminov', 'admin@mail.com', '$2a$10$Xl0yhvzLIaJCDdKBS0Lld.ksK7c2Zytg/ZKFdtIYYQUv8rUfvCR4W', 'ADMIN'),
+    ('User', 'Userov', 'user@mail.com', '$2a$10$Xl0yhvzLIaJCDdKBS0Lld.ksK7c2Zytg/ZKFdtIYYQUv8rUfvCR4W', 'USER');
