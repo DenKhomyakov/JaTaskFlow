56d5fb19a78651a5f38a8015e4f00c0a89e7801a
Denis Khomyakov <Homyakov.Denis2021@yandex.ru>
2025-03-30 22:32:00 +0700

    Added utilities for authorization

---
+++ JaTaskFlow/src/main/java/com/example/jataskflow/security/JwtTokenUtils.java
@@ -0,0 +1,48 @@
+package com.example.jataskflow.security;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtTokenUtils {
+
+    @Value("${jwt.access.secret}")
+    private String accessSecret;
+
+    @Value("${jwt.refresh.secret}")
+    private String refreshSecret;
+
+    @Value("${jwt.access.expiration}")
+    private Long accessExpiration;
+
+    @Value("${jwt.refresh.expiration}")
+    private Long refreshExpiration;
+
+    public String generateAccessToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("roles", userDetails.getAuthorities());
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + accessExpiration))
+                .signWith(SignatureAlgorithm.HS256, accessSecret)
+                .compact();
+    }
+
+    public String generateRefreshToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + refreshExpiration))
+                .signWith(SignatureAlgorithm.HS256, refreshSecret)
+                .compact();
+    }
+}
