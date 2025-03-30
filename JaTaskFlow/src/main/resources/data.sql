dd10e5829bfffd44416d24231093cfe0250416c0
Denis Khomyakov <Homyakov.Denis2021@yandex.ru>
2025-03-30 21:55:47 +0700

    Added the Security Configuration

--- JaTaskFlow/src/main/resources/data.sql
+++ JaTaskFlow/src/main/resources/data.sql
@@ -1,4 +1,15 @@
-INSERT INTO user (firstname, lastname, email, password, role)
-VALUES
-    ('Admin', 'Adminov', 'admin@mail.com', '$2a$10$Xl0yhvzLIaJCDdKBS0Lld.ksK7c2Zytg/ZKFdtIYYQUv8rUfvCR4W', 'ADMIN'),
-    ('User', 'Userov', 'user@mail.com', '$2a$10$Xl0yhvzLIaJCDdKBS0Lld.ksK7c2Zytg/ZKFdtIYYQUv8rUfvCR4W', 'USER');
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL
+    );
+
+INSERT INTO users (firstname, lastname, email, password, role)
+VALUES ('Admin', 'Adminov', 'admin@mail.com', '$2a$10$Xl0yhvzLIaJCDdKBS0Lld.ksK7c2Zytg/ZKFdtIYYQUv8rUfvCR4W', 'ADMIN');
+
+
+INSERT INTO users (firstname, lastname, email, password, role)
+VALUES ('User', 'Userov', 'user@mail.com', '$2a$10$Xl0yhvzLIaJCDdKBS0Lld.ksK7c2Zytg/ZKFdtIYYQUv8rUfvCR4W', 'USER');
