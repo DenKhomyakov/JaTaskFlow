7c0af15dc92e1769da8c6bf36256d0e74b080ce9
Denis Khomyakov <Homyakov.Denis2021@yandex.ru>
2025-03-30 19:42:09 +0700

    A project has been created and its functionality has been checked

---
+++ JaTaskFlow/src/main/java/com/example/jataskflow/JaTaskFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.jataskflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaTaskFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaTaskFlowApplication.class, args);
+	}
+
+}
