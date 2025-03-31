39ee494fde7dc712a732414075e69296dbc25273
Denis Khomyakov <Homyakov.Denis2021@yandex.ru>
2025-03-31 19:51:53 +0700

    Replaced the use of int and long with Long

--- JaTaskFlow/src/main/java/com/example/jataskflow/model/User.java
+++ JaTaskFlow/src/main/java/com/example/jataskflow/model/User.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(nullable = false)
     private String firstname;
@@ -41,11 +41,11 @@ public class User implements UserDetails {
         this.password = password;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
