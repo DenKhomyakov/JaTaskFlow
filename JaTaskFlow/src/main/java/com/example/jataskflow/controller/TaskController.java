88fb5df992863afd76612bf3c4d73f490bc4a9bf
Denis Khomyakov <Homyakov.Denis2021@yandex.ru>
2025-03-31 16:39:09 +0700

    Added the Task controller

---
+++ JaTaskFlow/src/main/java/com/example/jataskflow/controller/TaskController.java
@@ -0,0 +1,30 @@
+package com.example.jataskflow.controller;
+
+import com.example.jataskflow.dto.TaskDto;
+import com.example.jataskflow.model.Task;
+import com.example.jataskflow.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+    private final TaskService taskService;
+
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody TaskDto taskDto) {
+        return taskService.createTask(taskDto);
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+}
