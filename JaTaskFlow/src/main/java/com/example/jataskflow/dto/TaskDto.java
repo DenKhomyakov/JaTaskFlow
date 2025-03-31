a5bfb72ce69655c18eb25d5cc43370713c570e4b
Denis Khomyakov <Homyakov.Denis2021@yandex.ru>
2025-03-31 16:34:29 +0700

    Added the Task data transfer object

---
+++ JaTaskFlow/src/main/java/com/example/jataskflow/dto/TaskDto.java
@@ -0,0 +1,61 @@
+package com.example.jataskflow.dto;
+
+import com.example.jataskflow.model.Priority;
+import com.example.jataskflow.model.Status;
+
+public class TaskDto {
+    private String title;
+    private String description;
+    private Status status;
+    private Priority priority;
+    private Long authorId;
+    private Long executorId;
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Priority getPriority() {
+        return this.priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public Long getAuthorId() {
+        return this.authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+
+    public Long getExecutorId() {
+        return this.executorId;
+    }
+
+    public void setExecutorId(Long executorId) {
+        this.executorId = executorId;
+    }
+}
