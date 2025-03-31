facfe7cff8386be457a438b8b8f20858d13f5780
Denis Khomyakov <Homyakov.Denis2021@yandex.ru>
2025-03-31 17:05:52 +0700

    Added the Comment data transfer object

---
+++ JaTaskFlow/src/main/java/com/example/jataskflow/dto/CommentDto.java
@@ -0,0 +1,51 @@
+package com.example.jataskflow.dto;
+
+import java.time.LocalDateTime;
+
+public class CommentDto {
+    private Long id;
+    private String text;
+    private Long authorId;
+    private Long taskId;
+    private LocalDateTime createdAt;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Long getAuthorId() {
+        return this.authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+
+    public Long getTaskId() {
+        return this.taskId;
+    }
+
+    public void setTaskId(Long taskId) {
+        this.taskId = taskId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
