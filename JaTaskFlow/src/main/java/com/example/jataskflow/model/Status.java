0024546cd75ea0bee0fdb5202559cf00fd17f2b0
Denis Khomyakov <Homyakov.Denis2021@yandex.ru>
2025-03-31 16:04:48 +0700

    Added the task status enumeration

---
+++ JaTaskFlow/src/main/java/com/example/jataskflow/model/Status.java
@@ -0,0 +1,8 @@
+package com.example.jataskflow.model;
+
+public enum Status {
+    WAITING,      // Ожидает выполнения
+    IN_PROGRESS,  // В работе
+    COMPLETED,    // Завершена
+    REJECTED      // Отклонена
+}
