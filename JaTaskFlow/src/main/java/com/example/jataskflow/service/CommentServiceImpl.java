b6a65b2f14120919afe33e790307b7589c1525a4
Denis Khomyakov <Homyakov.Denis2021@yandex.ru>
2025-04-01 12:35:50 +0700

    Added the access denied exception

--- JaTaskFlow/src/main/java/com/example/jataskflow/service/CommentServiceImpl.java
+++ JaTaskFlow/src/main/java/com/example/jataskflow/service/CommentServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.jataskflow.service;
 import com.example.jataskflow.dto.CommentDto;
 import com.example.jataskflow.dto.request.CommentRequest;
 import com.example.jataskflow.dto.response.CommentResponse;
+import com.example.jataskflow.exception.AccessDeniedException;
 import com.example.jataskflow.exception.NotFoundException;
 import com.example.jataskflow.model.Comment;
 import com.example.jataskflow.model.Role;
